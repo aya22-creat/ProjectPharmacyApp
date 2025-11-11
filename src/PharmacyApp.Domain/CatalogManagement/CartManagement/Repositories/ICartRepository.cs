da97d40cc462f83066d6fb7680a812c43eeb9d7e
aya22-creat <ayawaleed05@gmail.com>
2025-11-11 17:28:08 +0200

    `Code refactoring and minor changes across multiple files`

--- src/PharmacyApp.Domain/CatalogManagement/CartManagement/Repositories/ICartRepository.cs
+++ src/PharmacyApp.Domain/CatalogManagement/CartManagement/Repositories/ICartRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using PharmacyApp.Domain.CatalogManagement.CartManagement.Entities;
 using PharmacyApp.Common.Common.Repositories;
 
 namespace PharmacyApp.Domain.CatalogManagement.CartManagement.Repositories
@@ -8,5 +9,9 @@ namespace PharmacyApp.Domain.CatalogManagement.CartManagement.Repositories
     public interface ICartRepository : IRepository<Entities.Cart>
     {
         Task<Entities.Cart?> GetActiveCartByCustomerIdAsync(Guid customerId, CancellationToken cancellationToken = default);
+
+        Task<bool> ExistsForCustomerAsync(Guid customerId, CancellationToken cancellationToken = default);
+
+       Task<Cart?> GetByCustomerIdAsync(Guid customerId, CancellationToken cancellationToken = default);
     }
 }
